146c5110d5e8dee35d11f58e7eac5031e69d1b50
unknown <kulinich.dima@yandex.ru>
2019-12-27 10:10:51 +0300

    fix

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 def get_result(in_data_a='in_data_a.csv', in_data_b='in_data_p.csv', out='out.csv'):
     """
+    Для решения задачи
     Получение информации о стоимости одной установки для каждой рекламной кампании, разбитую по названиям рекламируемых
     приложений, по операционной системе и по дням
     :param in_data_a: путь к файлу с информацией об установках
@@ -17,8 +18,8 @@ def get_result(in_data_a='in_data_a.csv', in_data_b='in_data_p.csv', out='out.cs
     df_in_data_a = pd.read_csv(in_data_a)
     df_in_data_b = pd.read_csv(in_data_b)
     length = int(df_in_data_a['id'].count())      # Для отображения прогресса
-    start = time.time()
 
+    start = time.time()
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', stream=sys.stdout)
     logging.info('Старт программы')
 
@@ -56,6 +57,64 @@ def get_result(in_data_a='in_data_a.csv', in_data_b='in_data_p.csv', out='out.cs
     df_result.to_csv(out, float_format='%.1f')
     logging.info(f'Программа окончена за {round(time.time() - start)} секунд(ы), обработано строк - {length}')
 
+########################################################################################################################
+class Work:
+    """
+    Реализация класса Не только сохраняет выполнене задачи, но и обеспечивает взаимод с Tkinter
+    """
+    def __init__(self, in_data_a='in_data_a.csv', in_data_b='in_data_p.csv', out='out.csv'):
+        self.out = out
+        self.df_in_data_a = pd.read_csv(in_data_a)
+        self.df_in_data_b = pd.read_csv(in_data_b)
+        self.length = int(self.df_in_data_a['id'].count())
+
+
+    def first(self, progress=None):
+        start = time.time()
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', stream=sys.stdout)
+        logging.info('Старт программы')
+        df_result = pd.DataFrame(dict(app=[], date=[], campaign=[], os=[], installs=[], spend=[], cpi=[]))
+
+        for i in self.df_in_data_a.index:
+            if i % 1000 == 0:
+                logging.info(f'обрабатано {round(i /  self.length * 100)}% информации')
+                status = round(i / self.length * 100)
+                if progress is not None and status != 0:
+                    progress.set(status)
+            try:
+                app = self.df_in_data_a.loc[i, 'app']
+                date = self.df_in_data_a.loc[i, 'Date']
+                campaign = self.df_in_data_a.loc[i, 'Campaign']
+                os = self.df_in_data_a.loc[i, 'os']
+                installs = int(self.df_in_data_a.loc[i, 'Installs'])
+                spend = self.df_in_data_b[(self.df_in_data_b['ad_id'] == self.df_in_data_a.loc[i, 'ad_id']) &
+                                     (self.df_in_data_b['date'] == date)]['spend']
+                if spend.empty:
+                    spend = 0
+                else:
+                    spend = round(float(spend), 2)
+                try:
+                    cpi = round(float(spend) / installs, 2)
+                except ZeroDivisionError:
+                    cpi = 0
+                df_result = df_result.append(dict(app=app, date=date, campaign=campaign, os=os, installs=installs,
+                                                  spend=spend, cpi=cpi), ignore_index=True)
+            except Exception as e:
+                logging.error(f'Непредвиденная ошибка {e}')
+                raise
+
+        df_result = df_result.groupby(['app', 'date', 'campaign', 'os']).sum()
+        convert_dict = {'installs': int,
+                        'spend': float,
+                        'cpi': float,
+                        }
+        df_result = df_result.astype(convert_dict)
+        df_result.to_csv(self.out, float_format='%.1f')
+        logging.info(f'Программа окончена за {round(time.time() - start)} секунд(ы), обработано строк - {self.length}')
+        progress.set(0)
+
 if __name__ == '__main__':
     get_result()
+    # a = Work()
+    # a.first()
 
--- main_tk.pyw
+++ main_tk.pyw
@@ -1,7 +1,4 @@
 import sys
-import time
-import logging
-import functools
 import threading
 
 import tkinter
@@ -9,14 +6,14 @@ import tkinter.scrolledtext
 import tkinter.ttk
 import tkinter.messagebox
 
-from main import get_result
+from main import get_result, Work
 
 
 class StdoutRedirector(object):
 
     def __init__(self, text_area, obj_self):
         self.text_area = text_area
-        self.obj_self =obj_self
+        self.obj_self = obj_self
 
     def write(self, str):
         self.text_area.insert(tkinter.END, str)
@@ -25,22 +22,23 @@ class StdoutRedirector(object):
 
 
 class Application(tkinter.ttk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, work=None, master=None):
         super().__init__(master)
+        self.work = work
         self.grid(sticky="w, n, e, s", padx=4, pady=4)
         self.create_widgets()
         self.master.title("Test")
         self.master.grid_rowconfigure(0, weight=1)
         self.master.grid_columnconfigure(0, weight=1)
-        self.create_widgets()
         orig_stdout = sys.stdout
 
 
     def create_widgets(self):
         self.master.option_add('*tearOff', 'FALSE')
+        self.pgb_value = tkinter.IntVar()
 
         self.btnGo = tkinter.ttk.Button(self, width=20, text="Запуск")
-        self.btnGo["command"] = threading.Thread(target=get_result, args=[]).start
+        self.btnGo["command"] = threading.Thread(target=self.work.first, kwargs={'progress': self.pgb_value}).start
         self.btnGo.grid(row=0, column=0)
 
         self.btnStop = tkinter.ttk.Button(self, width=20, text="Cтоп")
@@ -48,20 +46,23 @@ class Application(tkinter.ttk.Frame):
         self.btnStop.state(["disabled"])  # нужна доработка
         self.btnStop.grid(row=1, column=0)
 
-        self.info = tkinter.Text(root, wrap='word')
+        self.info = tkinter.Text(self, wrap='word')
         self.info.configure(font='TkFixedFont')
         self.info.grid(row=0, column=1, rowspan=3, sticky='w, n, e, s')
         sys.stdout = StdoutRedirector(self.info, self)
 
+        self.pgb = tkinter.ttk.Progressbar(self, maximum=100, variable=self.pgb_value)
+        self.pgb.grid(row=4, column=1, sticky='w, e')
 
         self.grid_rowconfigure(0, pad=5)
         self.grid_rowconfigure(1, pad=5)
-        self.grid_rowconfigure(2, pad=5, weight=1,)
+        self.grid_rowconfigure(2, pad=5, weight=1)
+        self.grid_rowconfigure(3, pad=5)
         self.grid_columnconfigure(0, pad=5, minsize=20)
         self.grid_columnconfigure(1, weight=1, pad=5)
 
-
+work = Work()
 root = tkinter.Tk()
-app = Application(master=root)
+app = Application(work=work, master=root)
 root.mainloop()
 
