cfcd599363359a4a8e680f7899b15f108b9a508b
unknown <kulinich.dima@yandex.ru>
2020-01-12 13:43:03 +0300

    some fix

--- main.py
+++ main.py
@@ -4,72 +4,25 @@ import time
 
 import pandas as pd
 
-
-def get_result(in_data_a='in_data_a.csv', in_data_b='in_data_p.csv', out='out.csv'):
-    """
-    Для решения задачи
-    Получение информации о стоимости одной установки для каждой рекламной кампании, разбитую по названиям рекламируемых
-    приложений, по операционной системе и по дням
-    :param in_data_a: путь к файлу с информацией об установках
-    :param in_data_b: путь к файлу с информацией о тратах
-    :param out: путь к выходному файлу
-    """
-    df_result = pd.DataFrame(dict(app=[], date=[], campaign=[], os=[], installs=[], spend=[], cpi=[]))
-    df_in_data_a = pd.read_csv(in_data_a)
-    df_in_data_b = pd.read_csv(in_data_b)
-    length = int(df_in_data_a['id'].count())      # Для отображения прогресса
-
-    start = time.time()
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', stream=sys.stdout)
-    logging.info('Старт программы')
-
-    for i in df_in_data_a.index:
-        if i % 1000 == 0:
-            logging.info(f'обрабатано {round(i / length * 100)}% информации')
-        try:
-            app = df_in_data_a.loc[i, 'app']
-            date = df_in_data_a.loc[i, 'Date']
-            campaign = df_in_data_a.loc[i, 'Campaign']
-            os = df_in_data_a.loc[i, 'os']
-            installs = int(df_in_data_a.loc[i, 'Installs'])
-            spend = df_in_data_b[(df_in_data_b['ad_id'] == df_in_data_a.loc[i, 'ad_id']) &
-                                 (df_in_data_b['date'] == date)]['spend']
-            if spend.empty:
-                spend = 0
-            else:
-                spend = round(float(spend), 2)
-            try:
-                cpi = round(float(spend) / installs, 2)
-            except ZeroDivisionError:
-                cpi = 0
-            df_result = df_result.append(dict(app=app, date=date, campaign=campaign, os=os, installs=installs,
-                                              spend=spend, cpi=cpi), ignore_index=True)
-        except Exception as e:
-            logging.error(f'Непредвиденная ошибка {e}')
-            raise
-
-    df_result = df_result.groupby(['app', 'date', 'campaign', 'os']).sum()
-    convert_dict = {'installs': int,
-                    'spend': float,
-                    'cpi': float,
-                    }
-    df_result = df_result.astype(convert_dict)
-    df_result.to_csv(out, float_format='%.1f')
-    logging.info(f'Программа окончена за {round(time.time() - start)} секунд(ы), обработано строк - {length}')
-
-########################################################################################################################
 class Work:
     """
     Реализация класса Не только сохраняет выполнене задачи, но и обеспечивает взаимод с Tkinter
     """
-    def __init__(self, in_data_a='in_data_a.csv', in_data_b='in_data_p.csv', out='out.csv'):
+    def __init__(self, in_data_a='in_data_a.csv', in_data_b='in_data_p.csv', out='out.csv', ):
         self.out = out
         self.df_in_data_a = pd.read_csv(in_data_a)
         self.df_in_data_b = pd.read_csv(in_data_b)
         self.length = int(self.df_in_data_a['id'].count())
+        self._running = None
 
+    def terminate(self):
+        if self._running:
+            self._running = False
+        else:
+            logging.info('Программа не запущена')
 
-    def first(self, progress=None):
+    def first(self, progress=None, running=True):
+        self._running = running
         start = time.time()
         logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', stream=sys.stdout)
         logging.info('Старт программы')
@@ -81,6 +34,8 @@ class Work:
                 status = round(i / self.length * 100)
                 if progress is not None and status != 0:
                     progress.set(status)
+            if not self._running:  # остановка
+                break
             try:
                 app = self.df_in_data_a.loc[i, 'app']
                 date = self.df_in_data_a.loc[i, 'Date']
@@ -106,15 +61,13 @@ class Work:
         df_result = df_result.groupby(['app', 'date', 'campaign', 'os']).sum()
         convert_dict = {'installs': int,
                         'spend': float,
-                        'cpi': float,
-                        }
+                        'cpi': float,}
         df_result = df_result.astype(convert_dict)
         df_result.to_csv(self.out, float_format='%.1f')
-        logging.info(f'Программа окончена за {round(time.time() - start)} секунд(ы), обработано строк - {self.length}')
+        logging.info(f'Программа окончена за {round(time.time() - start)} секунд(ы), обработано строк - {i}')
         progress.set(0)
 
 if __name__ == '__main__':
-    get_result()
-    # a = Work()
-    # a.first()
+    a = Work()
+    a.first()
 
--- main_tk.pyw
+++ main_tk.pyw
@@ -1,16 +1,17 @@
 import sys
 import threading
 
+import concurrent.futures as cf
+
 import tkinter
 import tkinter.scrolledtext
 import tkinter.ttk
 import tkinter.messagebox
 
-from main import get_result, Work
+from main import Work
 
 
 class StdoutRedirector(object):
-
     def __init__(self, text_area, obj_self):
         self.text_area = text_area
         self.obj_self = obj_self
@@ -19,6 +20,9 @@ class StdoutRedirector(object):
         self.text_area.insert(tkinter.END, str)
         self.text_area.see(tkinter.END)
         self.obj_self.update_idletasks()
+
+    # def flush(self):
+    #     pass
 
 
 class Application(tkinter.ttk.Frame):
@@ -30,6 +34,7 @@ class Application(tkinter.ttk.Frame):
         self.master.title("Test")
         self.master.grid_rowconfigure(0, weight=1)
         self.master.grid_columnconfigure(0, weight=1)
+
         orig_stdout = sys.stdout
 
 
@@ -38,13 +43,17 @@ class Application(tkinter.ttk.Frame):
         self.pgb_value = tkinter.IntVar()
 
         self.btnGo = tkinter.ttk.Button(self, width=20, text="Запуск")
-        self.btnGo["command"] = threading.Thread(target=self.work.first, kwargs={'progress': self.pgb_value}).start
+        self.btnGo["command"] =lambda: threading.Thread(target=self.work.first, kwargs={'progress': self.pgb_value, 'running': True}).start()
         self.btnGo.grid(row=0, column=0)
 
         self.btnStop = tkinter.ttk.Button(self, width=20, text="Cтоп")
-        self.btnStop["command"] = sys.exit
-        self.btnStop.state(["disabled"])  # нужна доработка
+        self.btnStop["command"] = self.work.terminate
         self.btnStop.grid(row=1, column=0)
+
+        self.btnExit = tkinter.ttk.Button(self, width=20, text="Выход")
+        self.btnExit["command"] = sys.exit
+        self.btnExit.state(["disabled"])  # нужна доработка
+        self.btnExit.grid(row=2, column=0)
 
         self.info = tkinter.Text(self, wrap='word')
         self.info.configure(font='TkFixedFont')
@@ -54,6 +63,9 @@ class Application(tkinter.ttk.Frame):
         self.pgb = tkinter.ttk.Progressbar(self, maximum=100, variable=self.pgb_value)
         self.pgb.grid(row=4, column=1, sticky='w, e')
 
+        sgp = tkinter.ttk.Sizegrip(self)
+        sgp.grid(row=4, column=2, sticky="e, s")
+
         self.grid_rowconfigure(0, pad=5)
         self.grid_rowconfigure(1, pad=5)
         self.grid_rowconfigure(2, pad=5, weight=1)
@@ -61,8 +73,50 @@ class Application(tkinter.ttk.Frame):
         self.grid_columnconfigure(0, pad=5, minsize=20)
         self.grid_columnconfigure(1, weight=1, pad=5)
 
+
+
+
+
 work = Work()
 root = tkinter.Tk()
 app = Application(work=work, master=root)
 root.mainloop()
 
+from threading import Thread
+import time
+
+class CountdownTask:
+    def __init__(self):
+        self._running = True
+
+    def terminate(self):
+        self._running = False
+
+    def run(self, n):
+        while self._running and n > 0:
+            print('T-minus', n)
+            n -= 1
+            time.sleep(5)
+
+c = CountdownTask()
+t = Thread(target=c.run, args=(10,))
+t.start()
+...
+c.terminate()   # Сигнал завершения
+t.join()        # Ждать реального завершения (если необходимо)
+
+
+# todo Иногда вы можете встретить определение потоков через наследование от класса Thread
+class CountdownThread(Thread):
+    def __init__(self, n):
+        super().__init__()
+        self.n = 0
+
+    def run(self):
+        while self.n > 0:
+            print('T-minus', self.n)
+            self.n -= 1
+            time.sleep(5)
+
+c = CountdownThread(5)
+c.start()
